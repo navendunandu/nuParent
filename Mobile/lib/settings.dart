981920809d2da61acb8b44df80fdbf3c2e289851
navendunandu <navendunandu@gmail.com>
2023-12-04 16:27:32 +0530

    Cirlce stack

--- Mobile/lib/settings.dart
+++ Mobile/lib/settings.dart
@@ -11,73 +11,211 @@ class SettingsScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
-                onPressed: () {},
-                icon: const Icon(Icons.arrow_back_ios_new), //Back Icon
-              ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.arrow_back_ios_new), //Back Icon
+        ),
       ),
       body: Container(
         decoration: const BoxDecoration(
-          image: DecorationImage(image: AssetImage('assets/Vector2.png'), fit: BoxFit.cover, alignment: Alignment.bottomCenter)
-        ),
+            image: DecorationImage(
+                image: AssetImage('assets/Vector2.png'),
+                fit: BoxFit.cover,
+                alignment: Alignment.bottomCenter)),
         child: ListView(
           children: [
-            const SizedBox(
-              height: 90,
+            SizedBox(
+              height: 140,
               child: Stack(
                 children: [
-                  Center(child: Text('Settings', style: TextStyle(fontSize: 26, fontWeight: FontWeight.w500, color: AppColors.darkblue),))
+                  Positioned(
+                    left: 90,
+                    top: 5,
+                    child: Container(
+                      width: 80,
+                      height: 80,
+                      decoration: const BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: Color.fromARGB(33, 0, 0, 0),
+                            spreadRadius: 1,
+                            blurRadius: 10,
+                            offset: Offset(0, 0),
+                          ),
+                        ],
+                        shape: BoxShape.circle,
+                        color: Color.fromARGB(255, 213, 232, 251),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 80,
+                    top: 5,
+                    child: Container(
+                      width: 18,
+                      height: 18,
+                      decoration: const BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: Color.fromARGB(33, 0, 0, 0),
+                            spreadRadius: 1,
+                            blurRadius: 10,
+                            offset: Offset(0, 0),
+                          ),
+                        ],
+                        shape: BoxShape.circle,
+                        color: Color.fromARGB(255, 213, 232, 251),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 92,
+                    top: 82,
+                    child: Container(
+                      width: 10,
+                      height: 10,
+                      decoration: const BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: Color.fromARGB(33, 0, 0, 0),
+                            spreadRadius: 1,
+                            blurRadius: 10,
+                            offset: Offset(0, 0),
+                          ),
+                        ],
+                        shape: BoxShape.circle,
+                        color: Color.fromARGB(255, 0, 131, 231),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 125,
+                    top: 75,
+                    child: Container(
+                      width: 35,
+                      height: 35,
+                      decoration: const BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: Color.fromARGB(33, 0, 0, 0),
+                            spreadRadius: 1,
+                            blurRadius: 10,
+                            offset: Offset(0, 0),
+                          ),
+                        ],
+                        shape: BoxShape.circle,
+                        color: Color.fromARGB(255, 99, 137, 165),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 40,
+                    top: 25,
+                    child: Container(
+                      width: 55,
+                      height: 55,
+                      decoration: const BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: Color.fromARGB(33, 0, 0, 0),
+                            spreadRadius: 1,
+                            blurRadius: 10,
+                            offset: Offset(0, 0),
+                          ),
+                        ],
+                        shape: BoxShape.circle,
+                        color: Color.fromARGB(255, 0, 82, 140),
+                      ),
+                    ),
+                  ),
+                  const Center(
+                      child: Text(
+                    'Settings',
+                    style: TextStyle(
+                        fontSize: 26,
+                        fontWeight: FontWeight.w500,
+                        color: AppColors.darkblue),
+                  ))
                 ],
               ),
             ),
             ListTile(
-              contentPadding: const EdgeInsets.all(6),
+              contentPadding: const EdgeInsets.all(2),
               tileColor: AppColors.tileprimaryblue,
-              title: const Text("Child's Details", textAlign: TextAlign.center,),
-              onTap: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context)=> const ViewProfile()));
+              title: const Text(
+                "Child's Details",
+                textAlign: TextAlign.center,
+              ),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const ViewProfile()));
               },
             ),
             ListTile(
-              contentPadding: const EdgeInsets.all(6),
+              contentPadding: const EdgeInsets.all(2),
               tileColor: AppColors.tilesecondaryblue,
-              title: const Text("How to Videos", textAlign: TextAlign.center,),
-              onTap: (){},
+              title: const Text(
+                "How to Videos",
+                textAlign: TextAlign.center,
+              ),
+              onTap: () {},
             ),
             ListTile(
-              contentPadding: const EdgeInsets.all(6),
+              contentPadding: const EdgeInsets.all(2),
               tileColor: AppColors.tileprimaryblue,
-              title: const Text("Remainders", textAlign: TextAlign.center,),
-              onTap: (){},
+              title: const Text(
+                "Remainders",
+                textAlign: TextAlign.center,
+              ),
+              onTap: () {},
             ),
             ListTile(
-              contentPadding: const EdgeInsets.all(6),
+              contentPadding: const EdgeInsets.all(2),
               tileColor: AppColors.tilesecondaryblue,
-              title: const Text("FAQs", textAlign: TextAlign.center,),
-              onTap: (){},
+              title: const Text(
+                "FAQs",
+                textAlign: TextAlign.center,
+              ),
+              onTap: () {},
             ),
             ListTile(
-              contentPadding: const EdgeInsets.all(6),
+              contentPadding: const EdgeInsets.all(2),
               tileColor: AppColors.tileprimaryblue,
-              title: const Text("Contact", textAlign: TextAlign.center,),
-              onTap: (){},
+              title: const Text(
+                "Contact",
+                textAlign: TextAlign.center,
+              ),
+              onTap: () {},
             ),
             ListTile(
-              contentPadding: const EdgeInsets.all(6),
+              contentPadding: const EdgeInsets.all(2),
               tileColor: AppColors.tilesecondaryblue,
-              title: const Text("Share", textAlign: TextAlign.center,),
-              onTap: (){},
+              title: const Text(
+                "Share",
+                textAlign: TextAlign.center,
+              ),
+              onTap: () {},
             ),
             ListTile(
-              contentPadding: const EdgeInsets.all(6),
+              contentPadding: const EdgeInsets.all(2),
               tileColor: AppColors.tileprimaryblue,
-              title: const Text("Legal", textAlign: TextAlign.center,),
-              onTap: (){},
+              title: const Text(
+                "Legal",
+                textAlign: TextAlign.center,
+              ),
+              onTap: () {},
             ),
             ListTile(
-              contentPadding: const EdgeInsets.all(6),
+              contentPadding: const EdgeInsets.all(2),
               tileColor: AppColors.tilesecondaryblue,
-              title: const Text("Rate", textAlign: TextAlign.center,),
-              onTap: (){},
+              title: const Text(
+                "Rate",
+                textAlign: TextAlign.center,
+              ),
+              onTap: () {},
             )
           ],
         ),
--- Mobile/lib/view_profile.dart
+++ Mobile/lib/view_profile.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:nu_parent/Components/wavebg.dart';
 import 'package:nu_parent/main.dart';
 
 class ViewProfile extends StatefulWidget {
@@ -19,44 +18,205 @@ class _ViewProfileState extends State<ViewProfile> {
           icon: const Icon(Icons.arrow_back_ios_new), //Back Icon
         ),
       ),
-      body: Stack(
-        children: [ 
-          const Positioned(
-          bottom: 0,
-          child: WaveBackground(color: Color.fromARGB(255, 125, 166, 199)),
-        ),
-          SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.only(left: 25.0, right: 25.0),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: Container(
+        height: double.infinity,
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/Vector2.png'),
+                fit: BoxFit.cover,
+                alignment: Alignment.bottomCenter)),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        'View Profile',
+                        style: TextStyle(
+                            fontSize: 24, fontWeight: FontWeight.w500),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {},
+                        // style: const ButtonStyle(backgroundColor: AppColors.white),
+                        child: const Text('Edit'),
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Elevate
+                      ElevatedButton.icon(
+                        style: const ButtonStyle(
+                          backgroundColor: MaterialStatePropertyAll(AppColors.white)
+                        ),
+                          onPressed: () {},
+                          icon: const Icon(Icons.person_add_alt_1_outlined),
+                          label: const Text('Add another profile'))
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Container(
+              height: 180,
+              decoration: BoxDecoration(color: Colors.blue[100]),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+                child: Row(
                   children: [
-                    const Text(
-                      'View Profile',
-                      style: TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
+                    Stack(children: [
+                      Container(
+                        padding: const EdgeInsets.all(6),
+                        decoration: const BoxDecoration(
+                          color: Color.fromARGB(255, 216, 225, 233),
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                        ),
+                        child: Stack(children: [
+                          ClipRRect(
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(10)),
+                            child: Image.asset(
+                              'assets/dummy-profile-pic.png',
+                              height: 90,
+                            ),
+                          ),
+                        ]),
+                      ),
+                      Positioned(
+                        left: 63,
+                        top: 63,
+                        child: IconButton(
+                            iconSize: 20,
+                            padding: const EdgeInsets.all(5),
+                            onPressed: () {},
+                            style: const ButtonStyle(
+                              backgroundColor: MaterialStatePropertyAll(
+                                  Color.fromARGB(106, 58, 58, 58)),
+                              minimumSize: MaterialStatePropertyAll(Size(8, 8)),
+                            ),
+                            color: const Color.fromARGB(255, 185, 185, 185),
+                            icon: const Icon(
+                              Icons.camera_alt_rounded,
+                              color: AppColors.black,
+                            )),
+                      )
+                    ]),
+                    const SizedBox(
+                      width: 10,
                     ),
-                    ElevatedButton(
-                      onPressed: () {},
-                      // style: const ButtonStyle(backgroundColor: AppColors.white),
-                      child: const Text('Edit'),
-                    )
+                    const Text(
+                      'Cia Anumod',
+                      style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.w500,
+                          color: AppColors.black),
+                    ),
+                    // DropdownButton(items: const [], onChanged: (Object? value) { print('hi'); },),
+                    IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.keyboard_arrow_down_outlined,
+                          color: AppColors.black,
+                        ))
                   ],
                 ),
-                Row(
-                  children: [
-                    ElevatedButton.icon(
-                        onPressed: () {},
-                        icon: const Icon(Icons.person_add_alt_1_outlined),
-                        label: const Text('Add another profile'))
-                  ],
-                )
-              ],
+              ),
             ),
-          ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Child Details',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Age',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.grey,
+                                  fontSize: 18),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              'Date of Birth',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.grey,
+                                  fontSize: 18),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              'Gender',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.grey,
+                                  fontSize: 18),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          width: 50,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              '6 Years',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w500, fontSize: 18),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              '23/06/2016',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w500, fontSize: 18),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              'Female',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w500, fontSize: 18),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            )
+          ],
         ),
-        ]
       ),
     );
   }
