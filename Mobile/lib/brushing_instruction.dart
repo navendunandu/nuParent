67f6d1b939219e1c18b3ace1941b7ae6f566acbb
navendunandu <navendunandu@gmail.com>
2023-12-04 18:26:40 +0530

    Brushing Instruction

---
+++ Mobile/lib/brushing_instruction.dart
@@ -0,0 +1,192 @@
+import 'package:flutter/material.dart';
+import 'package:nu_parent/main.dart';
+
+class BrushingInstruction extends StatefulWidget {
+  const BrushingInstruction({super.key});
+
+  @override
+  State<BrushingInstruction> createState() => _BrushingInstructionState();
+}
+
+class _BrushingInstructionState extends State<BrushingInstruction> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/Vector3.png'),
+                fit: BoxFit.cover,
+                alignment: Alignment.topCenter)),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 10.0, right: 10.0, top: 25),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          icon: const Icon(Icons.arrow_back_ios_new)),
+                      IconButton(
+                          onPressed: () {},
+                          icon: const Icon(Icons.more_vert_rounded)),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    ClipRRect(
+                      borderRadius: const BorderRadius.all(Radius.circular(100)),
+                      child: Image.asset('assets/Teeths.png'),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Text('Brushing Instruction', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: AppColors.darkblue),),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          height: 150,
+                          width: 150,
+                          decoration: BoxDecoration(
+                              color: AppColors.lightblue,
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(10)),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black.withOpacity(0.2),
+                                  spreadRadius: 1,
+                                  blurRadius: 5,
+                                  offset: const Offset(0, 2),
+                                )
+                              ]),
+                          child: const Center(
+                              child: Text(
+                            'Preparing to Brush',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: AppColors.white,
+                                fontWeight: FontWeight.w500,
+                                fontSize: 18),
+                          )),
+                        ),
+                        Container(
+                          height: 150,
+                          width: 150,
+                          decoration: BoxDecoration(
+                              color: AppColors.lightblue,
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(10)),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black.withOpacity(0.2),
+                                  spreadRadius: 1,
+                                  blurRadius: 5,
+                                  offset: const Offset(0, 2),
+                                )
+                              ]),
+                          child: const Center(
+                              child: Text(
+                            'Before Brushing',
+                            style: TextStyle(
+                                color: AppColors.white,
+                                fontWeight: FontWeight.w500,
+                                fontSize: 18),
+                          )),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        const BrushingInstruction()));
+                          },
+                          child: Container(
+                            height: 150,
+                            width: 150,
+                            decoration: BoxDecoration(
+                                color: AppColors.lightblue,
+                                borderRadius:
+                                    const BorderRadius.all(Radius.circular(10)),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black.withOpacity(0.2),
+                                    spreadRadius: 1,
+                                    blurRadius: 5,
+                                    offset: const Offset(0, 2),
+                                  )
+                                ]),
+                            child: const Center(
+                                child: Text(
+                              'While Brushing',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  color: AppColors.white,
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 18),
+                            )),
+                          ),
+                        ),
+                        Container(
+                          height: 150,
+                          width: 150,
+                          decoration: BoxDecoration(
+                              color: AppColors.lightblue,
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(10)),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black.withOpacity(0.2),
+                                  spreadRadius: 1,
+                                  blurRadius: 5,
+                                  offset: const Offset(0, 2),
+                                )
+                              ]),
+                          child: const Center(
+                              child: Text(
+                            'After Brushing',
+                            style: TextStyle(
+                                color: AppColors.white,
+                                fontWeight: FontWeight.w500,
+                                fontSize: 18),
+                          )),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
