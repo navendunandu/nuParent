f23c0026611381a6f4335c39199346c53849c944
navendunandu <navendunandu@gmail.com>
2023-12-14 15:58:56 +0530

    Reminder

--- Mobile/lib/Reminder.dart
+++ Mobile/lib/Reminder.dart
@@ -17,42 +17,192 @@ class _ReminderState extends State<Reminder> {
           onPressed: () {
             Navigator.pop(context);
           },
-          icon: const Icon(Icons.arrow_back_ios_new), //Back Icon
+          icon: const Icon(Icons.arrow_back_ios_new),
         ),
       ),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 20,
+      body: Container(
+        height: double.infinity,
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+          image: AssetImage('assets/Vector-1.png'),
+          fit: BoxFit.scaleDown,
+          alignment: Alignment.bottomCenter,
+        )),
+        child: SingleChildScrollView(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(25.0, 0, 25.0, 0),
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Text('Reminder', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: AppColors.primaryColor),),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      color: Color.fromARGB(255, 220, 231, 253)
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(25.0),
+                      child: Column(
+                        children: [
+                          const Text('Brushing', style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 18
+                          ),),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Container(
+                            padding: const EdgeInsets.all(15),
+                            decoration: BoxDecoration(
+                              color: AppColors.white,
+                              borderRadius: BorderRadius.circular(25)
+                            ),
+                            child: const Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text('Morning', style: TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 16
+                                ),),
+                                Text('8:00 AM', style: TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 16
+                                ),)
+                              ],
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          Container(
+                            padding: const EdgeInsets.all(15),
+                            decoration: BoxDecoration(
+                              color: AppColors.white,
+                              borderRadius: BorderRadius.circular(25)
+                            ),
+                            child: const Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text('Evening', style: TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 16
+                                ),),
+                                Text('8:00 PM', style: TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 16
+                                ),)
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      color: Color.fromARGB(255, 220, 231, 253)
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(25.0),
+                      child: Column(
+                        children: [
+                          const Text('Toothbrush replacement', style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 18
+                          ),),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Container(
+                            padding: const EdgeInsets.all(15),
+                            decoration: BoxDecoration(
+                              color: AppColors.white,
+                              borderRadius: BorderRadius.circular(25)
+                            ),
+                            child: const Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text('Every 3 months', style: TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 16
+                                ),),
+                                Row(
+                                children: [
+                                  Icon(Icons.calendar_today_outlined),
+                                  SizedBox(
+                                    width: 10,
+                                  ),
+                                  Icon(Icons.notifications_none_outlined),
+                                ],
+                              )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      color: Color.fromARGB(255, 220, 231, 253)
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(25.0),
+                      child: Column(
+                        children: [
+                          const Text('Dental Visits', style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 18
+                          ),),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Container(
+                            padding: const EdgeInsets.all(15),
+                            decoration: BoxDecoration(
+                              color: AppColors.white,
+                              borderRadius: BorderRadius.circular(25)
+                            ),
+                            child: const Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text('Every 6 months', style: TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 16
+                                ),),
+                                Row(
+                                children: [
+                                  Icon(Icons.calendar_today_outlined),
+                                  SizedBox(
+                                    width: 10,
+                                  ),
+                                  Icon(Icons.notifications_none_outlined),
+                                ],
+                              )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ),
+                ],
               ),
-              const Text('Reminder', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: AppColors.darkblue),),
-              const SizedBox(
-                height: 20,
-              ),
-              Container(
-                height: 180,
-                width: 350,
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                  color: Color.fromARGB(255, 134, 155, 173)
-                ),
-                child: const Center(child: Text('Daily, toothbrush replacement & dentist visit Reminders', textAlign: TextAlign.center, style: TextStyle(fontSize: 16),)),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Container(
-                height: 180,
-                width: 350,
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                  color: Color.fromARGB(255, 191, 216, 238)
-                ),
-                child: const Center(child: Text('Floss, Brush & Rinse Timers', textAlign: TextAlign.center, style: TextStyle(fontSize: 16),)),
-              )
-            ],
+            ),
           ),
         ),
       ),
